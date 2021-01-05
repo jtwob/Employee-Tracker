e5bd1ea9a1c4661fd7fe06dbee05b9e1565c3464
jtwob <32282322+jtwob@users.noreply.github.com>
2021-01-05 15:42:07 -0800

    UPDATE: added database/table templates to schema.sql

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS company;
+CREATE DATABASE company;
+
+USE company;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id),
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id),
+    FOREIGN KEY(manager_id),
+);
