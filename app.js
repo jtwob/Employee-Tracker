6ee88d2107feef5d647b358ebf101dd48789c3da
jtwob <32282322+jtwob@users.noreply.github.com>
2021-01-05 15:41:33 -0800

    UPDATE: Created array of rudimentary main menu

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+const mainMenu = [
+    "Add department", 
+    "Add role", 
+    "Add employee", 
+    "View departments", 
+    "View roles", 
+    "View employees", 
+    "Update employee department",
+    "Update employee roles",
+    "Update employee manager"
+];
+
